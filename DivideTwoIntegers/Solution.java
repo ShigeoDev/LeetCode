af08fe6255671e4ee6fcf2905f5a24b9783e948b
ShigeoDev <84653162+ShigeoDev@users.noreply.github.com>
2022-03-26 22:53:05 -0700

    bad solution to dividetwointegers

--- DivideTwoIntegers/Solution.java
+++ DivideTwoIntegers/Solution.java
@@ -1,5 +1,21 @@
 class Solution {
 	public int divide(int dividend, int divisor) {
+		boolean negative = false;
+		if (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0) {
+			negative = true;
+		}
+		long num = Long.divideUnsigned(Math.abs((long) dividend), Math.abs((long) divisor));
+		if (negative) {
+			if (-num < Integer.MIN_VALUE) {
+				return Integer.MIN_VALUE;
+			}
+			return (int) -num;
+		} else {
+			if (num > Integer.MAX_VALUE) {
+				return Integer.MAX_VALUE;
+			}
+			return (int) num;
+		}
 
 	}
 }
